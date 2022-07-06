65d2f66877e74eef766d1f3d3ce40ea669a68d75
Reynaldodomenico <reynaldodomenico@yahoo.com>
2022-07-06 21:06:27 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
-import React from 'react'
-import {Cards, Chart, CountryPicker} from './components'
-import styles from './App.module.css'
-import {fetchData } from './api'
+import React from 'react';
 
+import { Cards, CountryPicker, Chart } from './components';
+import { fetchData } from './api/';
+import styles from './App.module.css';
+
+import image from './images/image.png';
 
 class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
 
   async componentDidMount() {
     const data = await fetchData();
 
-    console.log(data)
+    this.setState({ data });
   }
 
-  render(){
-    return(
-      <div className ={styles.container}>
-      <Cards/>
-      <CountryPicker/>
-      <Chart/>
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} /> 
       </div>
-    )
+    );
   }
 }
 
-
-export default App
+export default App;
